a955e3c3dddc8d08b0fd865d85ccad7f87e70509
Mammuasyir <mammuasyir0101@gmailcom>
2021-12-13 20:13:54 +0700

    Fixed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Login</title>
